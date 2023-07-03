23d10c76824e00b32f411f5f206f21f7ecd7faf7
HibaBoutarkha <99880937+HibaBoutarkha@users.noreply.github.com>
2023-07-03 13:55:10 +0100

    Update data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,68 @@
+
+/*insert into customers(uuid,
+                      birth_address,
+                      birthdate,
+                      email,
+                      firstname,
+                      lastname,
+                      phone_number)
+values(2,
+       'street,city,state,postal,country',
+       '2000-05-01',
+       'email',
+       'firstname',
+       'lastname',
+       'phone_number');
+insert into accounts(
+    id,
+    account_number,
+    rib ,
+    is_active,
+    ownerID,
+    card_number,
+    card_expiration_date,
+    card_creation_date,
+    card_cvc,
+    card_is_enabled
+) values (1,
+          'account_number',
+          'rib',
+          true,
+          2,
+          'card_number',
+          '2000-05-01',
+          '2000-05-01',
+          'card_cvc',
+          true);
+*/
+/*insert into virtual_cards(
+    id,
+    card_number,
+    card_expiration_date,
+    card_creation_date,
+    card_cvc,
+    card_is_enabled,
+    accountid
+) values (1,
+          'v_card_number',
+          '2000-05-01',
+          '2000-05-01',
+          'card_cvc',
+          true,
+          'account_number');
+
+insert into virtual_cards(
+    id,
+    card_number,
+    card_expiration_date,
+    card_creation_date,
+    card_cvc,
+    card_is_enabled,
+    accountid
+) values (2,
+          'v_card_number2',
+          '2000-05-01',
+          '2000-05-01',
+          'card_cvc2',
+          true,
+          'account_number');*/
