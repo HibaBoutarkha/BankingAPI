b9c62d7212019538e04d1d52f20ecd49136cf7a5
Hiba <hiba.boutarkha.pro@gmail.com>
2023-07-03 14:02:59 +0100

    [BANKING SERVICE] customer.not.found exception handling

--- src/main/java/com/example/bankingservice/domain/business/apis/CustomerAPI.java
+++ src/main/java/com/example/bankingservice/domain/business/apis/CustomerAPI.java
@@ -4,10 +4,13 @@ import com.example.bankingservice.domain.base.Customer;
 import com.example.bankingservice.domain.business.usecases.GetCustomerDetails;
 import com.example.bankingservice.domain.business.usecases.GetCustomerDetailsRequest;
 import com.example.bankingservice.domain.dtoRepos.CustomerRepo;
+import com.example.bankingservice.domain.utils.CustomException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("banking-service/customers")
@@ -18,8 +21,9 @@ public class CustomerAPI implements GetCustomerDetails {
 
     @Override
     @GetMapping("/customer-details")
-    public ResponseEntity<Customer> handle(@ModelAttribute GetCustomerDetailsRequest request) {
-        Customer customer = customerRepo.findByID(request.getUUID()).get();
-        return ResponseEntity.status(HttpStatus.OK).body(customer);
+    public ResponseEntity<Customer> handle(@ModelAttribute GetCustomerDetailsRequest request) throws CustomException {
+        Optional<Customer> customer = customerRepo.findByID(request.getUUID());
+        if(customer.isPresent()) return ResponseEntity.status(HttpStatus.OK).body(customer.get());
+        throw new CustomException(400, "customer.not.found");
     }
 }
--- src/main/java/com/example/bankingservice/domain/business/usecases/GetCustomerDetails.java
+++ src/main/java/com/example/bankingservice/domain/business/usecases/GetCustomerDetails.java
@@ -1,9 +1,10 @@
 package com.example.bankingservice.domain.business.usecases;
 
 import com.example.bankingservice.domain.base.Customer;
+import com.example.bankingservice.domain.utils.CustomException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 public interface GetCustomerDetails {
-    public ResponseEntity<Customer> handle(@ModelAttribute GetCustomerDetailsRequest request);
+    public ResponseEntity<Customer> handle(@ModelAttribute GetCustomerDetailsRequest request) throws CustomException;
 }
